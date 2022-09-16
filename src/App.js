36af5c1d300700069a8911697271f5992f1a5646
jsn-lps <lpsjsn94@gmail.com>
2022-09-16 11:20:14 -0400

    css and framework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div id="page-box">
-        suh
+
+        <div id="header">
+          <p id="header-text-left">sup bitches</p>
+          <p id="header-text-right">a valorant training assistant</p>
+        </div>
+
+        <div id="content-wrapper">
+
+          <nav id="nav-bar">
+          </nav>
+
+          <div id="event-wrapper">
+
+            <div id="event-header">
+            <p id="challenge-id" >100 bots</p>
+            </div>
+
+            <div id="event-body">
+              <p id="event-text">do the bot stuff. words. lorem opsomom are you team vandal or team phantom?</p>
+            </div>
+
+          </div>
+
+        </div>
+
       </div>
     </div>
   );
